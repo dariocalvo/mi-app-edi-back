10875268198448597bed5b0b9d22d01790abee53
Dario Calvo <humildin2011@gmail.com>
2021-05-31 03:35:16 -0300

    Cabecera agregada

--- Index.php
+++ Index.php
@@ -1,3 +1,5 @@
 <?php
+    header("Access-Control-Allow-Origin: ttps://app-calvo-front.herokuapp.com/"); 
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
     echo "Bienvenido a PHP ";
 ?>
