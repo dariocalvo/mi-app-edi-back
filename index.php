521d5e3df95733067874c8e551d5791b19067a04
Dario Calvo <humildin2011@gmail.com>
2021-05-31 03:57:52 -0300

    headers agregados

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    header("Access-Control-Allow-Origin: https://app-calvo-front.herokuapp.com/Login.html"); 
+    header("Access-Control-Allow-Origin: https://app-calvo-front.herokuapp.com/"); 
     header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
     echo "Bienvenido a PHP ";
 ?>
