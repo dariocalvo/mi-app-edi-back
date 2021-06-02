3e4be3faa44c7bf25de9b32b3d01b2927246911a
Dario Calvo <humildin2011@gmail.com>
2021-06-01 23:46:56 -0300

    ...

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
      
     if (isset($_POST)){
         $nombre= $_POST['usuario'];
-        $contraseña= $_POST['pass']
+        $contraseña= $_POST['pass'];
         echo ('Hola '.$nombre.'. Su contraseña es: '.$pass);
     }else{
         echo "No se recibieron datos.";
