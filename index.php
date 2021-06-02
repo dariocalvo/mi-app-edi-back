5eaa09d5b970c5795a48b6718436919c8dbbfa82
Dario Calvo <humildin2011@gmail.com>
2021-06-02 03:31:59 -0300

    ...

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
+var_dump($_POST);
+/*
     if (isset($_POST['datos'])){
         echo ('Los datos llegaron con exito');
     }else{
         echo "No se recibieron datos.";
     }
+*/    
 ?>
