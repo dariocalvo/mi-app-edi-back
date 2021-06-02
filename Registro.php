a9daa69c27691ea2e793bed2726429d90519afc1
Dario Calvo <humildin2011@gmail.com>
2021-06-02 06:02:48 -0300

    ...

--- Registro.php
+++ Registro.php
@@ -1,19 +1,3 @@
 <?php
-/*
-    if (isset($_POST)){
-        var_dump($_POST);
-    }else{
-        echo 'No llegaron datos del usuario';
-    }
-*/
-//    if (isset($_FILES)){
-        var_dump($_FILES);
-        
-//    }else{
-//        echo 'No llego imágen';
-//    }
-    
-    
-    
-    
+    var_dump($_FILES);
 ?>
