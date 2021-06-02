f30f85a37da169e7cad32d0fcf76b235f1160a4d
Dario Calvo <humildin2011@gmail.com>
2021-06-02 06:13:23 -0300

    ...

--- Registro.php
+++ Registro.php
@@ -1,3 +1,16 @@
 <?php
-    var_dump($_FILES);
+    if (isset($_POST)){
+        var_dump($_POST);
+        echo 'Datos del usuario recibidos';
+    }else{
+        echo 'No hay datos de usuario';
+    }
+
+    if (isset($_FILES['avatar'])){
+        var_dump($_FILES);
+        echo 'se cargo una imagen';
+    }else{
+        echo 'No llego imagen con los datos';
+    }
+
 ?>
